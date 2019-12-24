a7fba07e07293ce86d30f45bfd4e7f498a1f21ef
RajeshKurella <rajesh.mcarjy@gmail.com>
2019-12-24 22:55:40 +0530

    commiting to localbranch

--- demo/src/main/java/com/rajeshkurell/springBootStart/CourceAPI.java
+++ demo/src/main/java/com/rajeshkurell/springBootStart/CourceAPI.java
@@ -9,6 +9,8 @@ public class CourceAPI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//git testing 
+		//ading line2
+		//change from localbranch
 		SpringApplication.run(CourceAPI.class, args);
 
 	}
